69c3c9bf1cde5022c67ada9cebbcd881a6e30594
Blamak <vlasherassanchez@gmail.com>
2021-03-16 12:20:06 +0100

    new Postman collection and SQL file

--- SpringRestAPI/src/main/resources/white_collar.sql
+++ SpringRestAPI/src/main/resources/white_collar.sql
@@ -1,24 +1,80 @@
-CREATE TABLE IF NOT EXISTS Shop(
-  shop_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(250) DEFAULT NULL,
-  capacity INTEGER DEFAULT NULL
-);
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: white_collar
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-CREATE TABLE IF NOT EXISTS Pictures(
-  id INTEGER PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(250) DEFAULT NULL,
-  author VARCHAR(250) DEFAULT NULL,
-  shop_id INTEGER DEFAULT NULL,
-  CONSTRAINT pictures_fk FOREIGN KEY (shop_id) REFERENCES Shop (shop_id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO Shop (name,capacity) VALUES ('Gracia', 5000);
-INSERT INTO Shop (name,capacity) VALUES ('Sant Marti', 941);
-INSERT INTO Shop (name,capacity) VALUES ('Eixample', 498);
-INSERT INTO Shop (name,capacity) VALUES ('Horta',3500);
-INSERT INTO Shop (name,capacity) VALUES ('Clot', 200);
+--
+-- Table structure for table `pictures`
+--
 
-INSERT INTO Pictures (title,author,shop_id) VALUES ('The Starry Night', 'Vincent van Gogh', 1);
-INSERT INTO Pictures (title,author,shop_id) VALUES ('Girl with a Pearl Earring','Johannes Vermeer',1);
-INSERT INTO Pictures (title,author,shop_id) VALUES ('The Last Supper','Leonaro da Vinci',2);
-INSERT INTO Pictures (title,author,shop_id) VALUES ('Blue (Moby Dick)','Jackson Pollock',2);
+DROP TABLE IF EXISTS `pictures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pictures` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(250) DEFAULT NULL,
+  `author` varchar(250) DEFAULT NULL,
+  `shop_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `pictures_fk` (`shop_id`),
+  CONSTRAINT `pictures_fk` FOREIGN KEY (`shop_id`) REFERENCES `shop` (`shop_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pictures`
+--
+
+LOCK TABLES `pictures` WRITE;
+/*!40000 ALTER TABLE `pictures` DISABLE KEYS */;
+INSERT INTO `pictures` VALUES (1,'The Starry Night','Vincent van Gogh',1),(2,'Girl with a Pearl Earring','Johannes Vermeer',1),(3,'The Last Supper','Leonaro da Vinci',2),(4,'Blue (Moby Dick)','Jackson Pollock',2),(5,'The Bathers','Paul Cézanne',1),(6,'Musicians','Caravaggio',3),(7,'The Sleepers','Gustave Courbet',4),(8,'The Great Wave off Kanagawa','Hokusai',2),(9,'Mona Lisa','Leonardo da Vinci',2),(10,'The Scream','Edvard Munch',1),(11,'Creation of Adam','Michelangelo',4),(12,'The Third of May','Franciso Goya',3),(13,'Olympia','Édouard Manet',3),(14,'School of Athens','Raphael',1),(15,'The Arnolfini Marriage','Jan van Eyck',4),(16,'The Birth of Venus','Sandro Boticelli',3),(17,'Portrait of Dora Maar','Pablo Picasso',4),(28,'The Crucifixon','Francisco de Zurbaran',2);
+/*!40000 ALTER TABLE `pictures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shop`
+--
+
+DROP TABLE IF EXISTS `shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `shop` (
+  `shop_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `capacity` int DEFAULT NULL,
+  PRIMARY KEY (`shop_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shop`
+--
+
+LOCK TABLES `shop` WRITE;
+/*!40000 ALTER TABLE `shop` DISABLE KEYS */;
+INSERT INTO `shop` VALUES (1,'Gracia',5000),(2,'Sant Martí',941),(3,'Eixample',498),(4,'Horta',3500),(5,'Clot',200),(6,'Barri Gòtic',642),(11,'Guinardónet',349),(12,'Carmelo',5847);
+/*!40000 ALTER TABLE `shop` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-16 12:19:19
